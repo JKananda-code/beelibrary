811781f51d1a3edd5271dadc5d56476989213f52
John Kananda <jkananda5@outlook.com>
2025-06-13 03:11:14 -0600

    morning on 13 June 2025-2 commit

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,15 +1,22 @@
 "use client";
 
 import Link from "next/link";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
-    <nav className="bg-cyan-400 text-black p-4">
+    <nav className="bg-cyan-100 text-black p-4">
       <div className="container mx-auto flex justify-between">
+        <div>
         <Link href="/">
-          <h1 className="text-2xl font-bold bg-yellow-500">Bee-Logo</h1>
+           <Image src="/Final-logo.png"
+                alt="Logo"
+                height={150}
+                width={100}
+                />
         </Link>
-        <div className="space-x-4 text-xl font-bold">
+        </div>
+        <div className="space-x-4 text-xl font-bold m-auto">
           <Link href="/about">About</Link>
           <Link href="/contact">Contact</Link>
           <Link href="/dashboard">Dashboard</Link>
