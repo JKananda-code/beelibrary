91bc26960e35aa0585522f7e9d499b2a1c3f3d7e
John Kananda <jkananda5@outlook.com>
2025-06-15 18:54:51 -0600

    pm  on 15 June pm added padding to Logo

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,17 +6,17 @@ import Image from "next/image";
 const Navbar = () => {
   return (
     <nav className="bg-cyan-100 text-black p-4">
-      <div className="container mx-auto flex justify-between">
+      <div className="container mx-auto flex justify-between pl-20">
         <div>
         <Link href="/">
            <Image src="/BeeLogo.jpeg"
                 alt="Logo"
-                height={150}
-                width={100}
+                height={100}
+                width={50}
                 />
         </Link>
         </div>
-        <div className="space-x-4 text-xl font-bold m-auto">
+        <div className="flex justify-end space-x-4 text-xl font-bold ">
           <Link href="/about">About</Link>
           <Link href="/contact">Contact</Link>
           <Link href="/dashboard">Dashboard</Link>
