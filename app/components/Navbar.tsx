148dc2b8fbfcd6642950bdf736862cb924a4017c
John Kananda <jkananda5@outlook.com>
2025-06-15 22:51:15 -0600

    pm revised 15 June pm ajusted Logo

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   return (
     <nav className="bg-cyan-200 text-black p-4">
       <div className="flex justify-between items-center py-4">
-        <div className="w-1/4">
+        <div className="w-1/4 pr-2 sm:pr-4 md;pr-8">
         <Link href="/">
            <Image src="/BeeLogo.jpeg"
                 alt="Logo"
